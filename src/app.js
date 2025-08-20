4c688880473f110694326640d7deef972dc44812
shadab khan <shadabkhan2910@gmail.com>
2025-08-20 21:59:36 +0530

    stillllll

--- src/app.js
+++ src/app.js
@@ -1,16 +1,5 @@
-const fs = require('fs');
-const puppeteerCacheDir = '/tmp/puppeteer-cache';
-if (!fs.existsSync(puppeteerCacheDir)) {
-    fs.mkdirSync(puppeteerCacheDir, { recursive: true });
-}
-process.env.PUPPETEER_CACHE_DIR = puppeteerCacheDir;
-
-console.log('Set PUPPETEER_CACHE_DIR to:', process.env.PUPPETEER_CACHE_DIR);
 const express = require('express')
 const { getScrapedRouter } = require('./router/getScraperResult')
-
-
-
 
 const app = express()
 const { authRouter } = require('./router/authenticate')
