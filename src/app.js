ac30ea3a6d06b64b0281322618d9ddd227ea32a2
shadab khan <shadabkhan2910@gmail.com>
2025-08-20 20:20:55 +0530

    render-config

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const connectDB = require('./config/database');
 
 require("dotenv").config();
 app.use(cors({
-    // origin: ' http://localhost:5173',
+    // origin: 'http://localhost:5173',
     origin: 'https://outrankengine-front.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type ', 'Authorization'],
