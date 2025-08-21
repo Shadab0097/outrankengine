0c089fc68f1beb7432edad589b25c3cb455e2ba6
shadab khan <shadabkhan2910@gmail.com>
2025-08-21 13:05:01 +0530

    sh

--- render-build.sh
+++ render-build.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
-# Exit on error
+#exit on error
+
 set -o errexit
 
-# Install dependencies
+#Install dependencies
+
 npm install
 
-# Build project if needed
-# npm run build
+# Uncomment this line if you need to build your project
 
-# Set Puppeteer cache directory to /tmp (safe for Render)
-export PUPPETEER_CACHE_DIR=/tmp/puppeteer
+#npm run build
+
+ #Ensure the Puppeteer cache directory exists
+
+PUPPETEER_CACHE_DIR=C:\Users\dilsh\.cache\puppeteer\chrome\win64-139.0.7258.66\chrome-win64\chrome.exe
+
 mkdir -p $PUPPETEER_CACHE_DIR
 
-# Install Chrome for Puppeteer
+#Install Puppeteer and download Chrome
+
 npx puppeteer browsers install chrome
+
+#Store/pull Puppeteer cache with build cache
+
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+
+echo "...Copying Puppeteer Cache from Build Cache"
+
+#Copying from the actual path where Puppeteer stores its Chrome binary
+
+cp -R C:\Users\dilsh\.cache\puppeteer\chrome\win64-139.0.7258.66\chrome-win64\chrome.exe $PUPPETEER_CACHE_DIR
+
+else
+echo "...Storing Puppeteer Cache in Build Cache"
+cp -R $PUPPETEER_CACHE_DIR C:\Users\dilsh\.cache\puppeteer\chrome\win64-139.0.7258.66\chrome-win64\chrome.exe
+fi 
+
+
